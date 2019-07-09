090f9d5197e58c636a0acff0a12a6925e0bf3727
brantley gilliard <brantleygilliard@gmail.com>
2019-07-09 10:17:11 -0500

    Initial Commit

---
+++ Top Stories/SourcesViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Top Stories
+//
+//  Created by SESP Walkup on 7/9/19.
+//  Copyright © 2019 SESP Walkup. All rights reserved.
+//
+
+import UIKit
+
+class SourcesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
