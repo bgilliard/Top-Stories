18273eddaad5a95d51328d039ba2ae2c0f8689b2
brantley gilliard <brantleygilliard@gmail.com>
2019-07-09 10:48:54 -0500

    Finished initial set up of SourcesTableView

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -18,9 +18,44 @@ class SourcesViewController: UITableViewController {
         super.viewDidLoad()
         self.title = "News Sources"
         let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
-        // Do any additional setup after loading the view.
+        if let url = URL(string:query){
+            if let data = try? Data(contentsOf: url){
+                let json = try! JSON(data:data)
+                if json["status"] == "ok" {
+                    parse(json: json)
+                    return
+                }
+            }
+        }
+        loadError()
     }
-
-
+    func parse(json: JSON){
+        for result in json["sources"].arrayValue {
+            let id = result["id"].stringValue
+            let name = result["name"].stringValue
+            let description = result["description"].stringValue
+            let source = ["id": id, "name": name, "description" : description]
+            sources.append(source)
+        }
+        tableView.reloadData()
+    }
+    func loadError()
+    {
+        let alert = UIAlertController(title: "Loading Error", message: "There was a problem loading the news feed", preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return sources.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = sources[indexPath.row]
+        cell.textLabel?.text = source["name"]
+        cell.detailTextLabel?.text = source["description"]
+        return cell
+    }
 }
 
